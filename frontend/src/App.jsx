91cb0a054a3d440553b9a4f1993c0a2e14e35a32
PRANALI NAPHADE <pranalinaphade15@gmail.com>
2026-05-02 19:02:51 +0530

    Update socket connection URL to production

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css"
 import io from "socket.io-client"
 import Editor from "@monaco-editor/react"
 
-const socket=io("http://localhost:5000")
+const socket=io("https://real-time-code-editor-iapx.onrender.com")
 
 const App = () => {
   const [joined,setJoined]=useState(false)//tells whether user joined or not
